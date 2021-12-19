4004681a2d87e04ac0996f02273cfda3aa88f1e6
Zuhaib <theshahzuhaib96@gmail.com>
2021-12-19 17:44:37 +0530

    Add packages

--- server.js
+++ server.js
@@ -5,9 +5,8 @@ const mongoose = require('mongoose');
 const User = require('./model/user');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-
-const JWT_SECRET =
-   'sdjkfh8923yhjdksbfma@#*(&@*!^#&@bhjb2qiuhesdbhjdsfg839ujkdhfjk';
+require('dotenv').config();
+const JWT_SECRET = process.env.VAL;
 
 mongoose.connect('mongodb://localhost:27017/login-app-db', {
    useNewUrlParser: true,
@@ -18,47 +17,6 @@ mongoose.connect('mongodb://localhost:27017/login-app-db', {
 const app = express();
 app.use('/', express.static(path.join(__dirname, 'static')));
 app.use(bodyParser.json());
-
-app.post('/api/register', async (req, res) => {
-   const { username, password: plainTextPassword } = req.body;
-
-   if (!username || typeof username !== 'string') {
-      return res.json({ status: 'error', error: 'Invalid username' });
-   }
-
-   if (!plainTextPassword || typeof plainTextPassword !== 'string') {
-      return res.json({ status: 'error', error: 'Invalid password' });
-   }
-
-   if (plainTextPassword.length < 5) {
-      return res.json({
-         status: 'error',
-         error: 'Password too small. Should be atleast 6 characters',
-      });
-   }
-
-   const password = await bcrypt.hash(plainTextPassword, 10);
-
-   try {
-      const response = await User.create({
-         username,
-         password,
-      });
-      console.log('User created successfully: ', response);
-   } catch (error) {
-      if (error.code === 11000) {
-         // duplicate key
-         return res.json({ status: 'error', error: 'Username already in use' });
-      }
-      throw error;
-   }
-
-   res.json({ status: 'ok' });
-});
-
-app.listen(9999, () => {
-   console.log('Server up at 9999');
-});
 
 app.post('/api/change-password', async (req, res) => {
    const { token, newpassword: plainTextPassword } = req.body;
@@ -117,4 +75,45 @@ app.post('/api/login', async (req, res) => {
    }
 
    res.json({ status: 'error', error: 'Invalid username/password' });
+});
+
+app.post('/api/register', async (req, res) => {
+   const { username, password: plainTextPassword } = req.body;
+
+   if (!username || typeof username !== 'string') {
+      return res.json({ status: 'error', error: 'Invalid username' });
+   }
+
+   if (!plainTextPassword || typeof plainTextPassword !== 'string') {
+      return res.json({ status: 'error', error: 'Invalid password' });
+   }
+
+   if (plainTextPassword.length < 5) {
+      return res.json({
+         status: 'error',
+         error: 'Password too small. Should be atleast 6 characters',
+      });
+   }
+
+   const password = await bcrypt.hash(plainTextPassword, 10);
+
+   try {
+      const response = await User.create({
+         username,
+         password,
+      });
+      console.log('User created successfully: ', response);
+   } catch (error) {
+      if (error.code === 11000) {
+         // duplicate key
+         return res.json({ status: 'error', error: 'Username already in use' });
+      }
+      throw error;
+   }
+
+   res.json({ status: 'ok' });
+});
+
+app.listen(9999, () => {
+   console.log('Server up at 9999');
 });
